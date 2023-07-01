a7ed02ee2565e13849e63dbd60a864f5ce83c8b7
Artem <artemprigarov@gmail.com>
2023-07-01 11:56:08 +0300

    connect to db

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import signup from "./routes/signup.js";
+import signin from "./routes/signin.js";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//   })
+// );
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
+app.use("/api/signup", signup);
+app.use("/api/signin", signin);
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server is running on port 5000");
-});
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("Server is running on port 5000");
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error.message);
+  });
